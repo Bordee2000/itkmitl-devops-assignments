76a7a849351e6700a7c37c3e80cdf1075951a0fb
Bordee Jakteerangkul <Bordee2000@gmail.com>
2021-11-12 12:20:25 +0000

    add 09

---
+++ assignments/09-deploy-helm-mongodb-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#Create MongoDB secret
+kubectl create secret generic bookinfo-dev-ratings-mongodb-secret --from-literal=mongodb-password=CHANGEME --from-literal=mongodb-root-password=CHANGEME
+
+#Crete configmap
+kubectl create configmap bookinfo-dev-ratings-mongodb-initdb --from-file=databases/ratings_data.json --from-file=databases/script.sh
+
+#Deploy MongoDB
+helm install -f k8s/helm-values/values-bookinfo-dev-ratings-mongodb.yaml bookinfo-dev-ratings-mongodb bitnami/mongodb --version 10.28.4
